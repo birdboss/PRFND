a4e388c4430e2502f7a3c7ac09adae7c81d7b9b5
birdboss <66156317+birdboss@users.noreply.github.com>
2020-12-22 17:17:37 -0800

    Add files via upload

---
+++ ass1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+           
+            while (num <= 10)
+            {
+                Console.Write(num);
+                Console.ReadLine();
+                num++;
+
+            }
+               
+                
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignment 3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] userNumbers = new int[10];
+            int counter = 0;
+
+            Console.WriteLine("Please enter 10 numbers.");
+            
+            for (counter = 0; counter < 10; counter++)
+            {
+                userNumbers[counter] = Convert.ToInt32(Console.ReadLine());
+            }    
+            
+
+            Console.WriteLine("The largest number was:\n");
+            
+            Array.Sort(userNumbers);
+                     
+            
+            Console.WriteLine("The numbers you chose are:\n ");
+            for (int i = 0; i < userNumbers.Length; i++)
+            {
+                Console.WriteLine(userNumbers[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
